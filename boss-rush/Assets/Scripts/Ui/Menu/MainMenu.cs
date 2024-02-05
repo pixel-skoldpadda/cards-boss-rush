db278d41f07db7d893dace339450e84bd2f472b3
Vladislav <legovladislav@gmail.com>
2024-02-05 17:58:44 +0300

    Добавил бэк в главное меню.

--- boss-rush/Assets/Scripts/Ui/Menu/MainMenu.cs
+++ boss-rush/Assets/Scripts/Ui/Menu/MainMenu.cs
@@ -9,7 +9,6 @@ namespace Ui.Menu
 {
     public class MainMenu : MonoBehaviour
     {
-        [SerializeField] private GameObject audioSettings;
         [SerializeField] private GameObject menu;
 
         private IGameStateService _gameStateService;
@@ -22,11 +21,6 @@ namespace Ui.Menu
             _gameStateService = gameStateService;
         }
         
-        private void Start()
-        {
-            audioSettings.SetActive(false);
-        }
-        
         public void OnNewGameButtonClicked()
         {
             PlayerPrefs.DeleteAll();
@@ -34,12 +28,6 @@ namespace Ui.Menu
             
             _gameStateService.State = new GameState();
             _gameStateMachine.Enter<LoadLevelState, string>(SceneConfig.GameScene);
-        }
-        
-        public void OnOptionsButtonClicked()
-        {
-            menu.SetActive(false);
-            audioSettings.SetActive(true);
         }
 
         public void OnExitButtonClicked()
